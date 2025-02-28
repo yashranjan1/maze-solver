008376bab7f02653d16d2f745956292fea3d2fba
Yash <yash.ranjan25@gmail.com>
2025-02-28 16:50:14 +0530

    changed the model for the cell once again, added a method to draw a path between two cells

--- objects/cell.py
+++ objects/cell.py
@@ -1,3 +1,4 @@
+import math
 from typing import TYPE_CHECKING
 
 from objects.line import Line
@@ -8,31 +9,43 @@ if TYPE_CHECKING:
 
 
 class Cell:
-    def __init__(self, win: "Window"):
+    def __init__(self, win: "Window", x1: int, y1: int, x2: int, y2: int) -> None:
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = None
-        self._x2 = None
-        self._y1 = None
-        self._y2 = None
-        self._win = win
-
-    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
+        self._win = win
+
+    def draw(self) -> None:
         if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1, y2))
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
             self._win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
             self._win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
+        color = "gray"
+        if not undo:
+            color = "red"
+
+        start = Point(
+            math.floor((self._x1 + self._x2) / 2), math.floor((self._y1 + self._y2) / 2)
+        )
+        end = Point(
+            math.floor((to_cell._x1 + to_cell._x2) / 2),
+            math.floor((to_cell._y1 + to_cell._y2) / 2),
+        )
+
+        line = Line(start, end)
+        self._win.draw_line(line, color)
